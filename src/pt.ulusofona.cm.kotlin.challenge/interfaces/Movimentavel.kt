c5d7918c3ca0ea09d39c2526f866299e91968d64
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2023-03-20 23:50:45 +0000

    continue

--- src/pt.ulusofona.cm.kotlin.challenge/interfaces/Movimentavel.kt
+++
@@ -1,7 +0,0 @@
-package pt.ulusofona.cm.kotlin.challenge.interfaces
-
-class Movimentavel {
-    fun moverPara(x:Int,y:Int){
-
-    }
-}
--- src/test/kotlin/exceptions.kt
+++
@@ -1,8 +0,0 @@
-class exceptions {
-//    AlterarPosicaoException { Quanto tentas um movimento para a posição onde já te encontras. }
-//    MenorDeIdadeException { Quando uma pessoa tenta tirar a carta e é menor de idade. }
-//    PessoaSemCartaException { Quanto uma pessoa sem carta tenta conduzir um veículo que necessita de carta. }
-//    VeiculoNaoEncontradoException { Quando pesquisas por um veículo que uma pessoa não possui. }
-//    VeiculoDesligadoException { Quanto uma pessoa tenta desligar um veículo e o mesmo já se encontra desligado. }
-//    VeiculoLigadoException { Quando uma pessoa tenta ligar um veículo e o mesmo já se encontra ligado. }
-}
